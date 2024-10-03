0607fb0f01c2f887106e4be3dd96428ea6865df8
Smruti535 <138751739+Smruti535@users.noreply.github.com>
2024-10-03 16:41:34 +0530

    Ecommerce Project Initial commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa corrupti eum commodi, incidunt nostrum rem. Expedita veritatis, corrupti soluta ut reiciendis aspernatur delectus. Ipsa tempora suscipit pariatur quis exercitationem, odio mollitia, beatae possimus atque eos cupiditate at est? Deleniti, sequi libero! Eaque ipsum repellendus at corporis ipsam, similique reiciendis, nesciunt reprehenderit dolorum quasi eos voluptatum sit, repudiandae quam in qui tempora aut repellat vitae. Recusandae voluptatum adipisci architecto ipsa doloribus qui dolorum delectus, ipsam doloremque est eos dolore natus quas temporibus. Esse possimus quasi ducimus deserunt qui quae, sint nemo?</p>
+            <p>E-commerce Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam voluptatem perferendis natus, officia laborum itaque aliquid quo, dignissimos explicabo quidem amet! Provident labore voluptas voluptate culpa! Veniam placeat doloremque quidem ipsam quia, quos quaerat reprehenderit iste fugit nam ullam?</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
